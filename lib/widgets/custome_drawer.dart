6dd0b860d4c2fb4c160e1691db84c58b123c03f4
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2024-04-27 13:11:08 +0300

    drawer items style

--- lib/widgets/custome_drawer.dart
+++ lib/widgets/custome_drawer.dart
@@ -1,45 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:responsive_dashboard/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
-import 'package:responsive_dashboard/widgets/drawer_item.dart';
+import 'package:responsive_dashboard/widgets/drawer_item_list_view.dart';
 import 'package:responsive_dashboard/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
-  static const List<DrawerItemModel> items = [
-    DrawerItemModel('Dashboard', Assets.imagesDashboard),
-    DrawerItemModel('My Transaction', Assets.imagesMyTransacktion),
-    DrawerItemModel('Statistics', Assets.imagesStatistics),
-    DrawerItemModel('Wallet Account', Assets.imagesWallet),
-    DrawerItemModel('My Investments', Assets.imagesSetting),
-  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
+      child: const Column(
         children: [
-          const UserInfoListTile(
+          UserInfoListTile(
             image: Assets.imagesInfoImage,
             title: "Abdulwahab",
             subTitle: "wahab@gmail.com",
           ),
-          const SizedBox(
+          SizedBox(
             height: 8,
           ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: DrawerItem(drawerItemModel: items[index]),
-              );
-            },
-          )
+          DrawerItemListView(),
         ],
       ),
     );
---
+++ lib/widgets/drawer_item_list_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
+import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/widgets/drawer_item.dart';
+
+class DrawerItemListView extends StatefulWidget {
+  const DrawerItemListView({
+    super.key,
+  });
+
+  @override
+  State<DrawerItemListView> createState() => _DrawerItemListViewState();
+}
+
+class _DrawerItemListViewState extends State<DrawerItemListView> {
+  //
+  final List<DrawerItemModel> items = [
+    const DrawerItemModel('Dashboard', Assets.imagesDashboard),
+    const DrawerItemModel('My Transaction', Assets.imagesMyTransacktion),
+    const DrawerItemModel('Statistics', Assets.imagesStatistics),
+    const DrawerItemModel('Wallet Account', Assets.imagesWallet),
+    const DrawerItemModel('My Investments', Assets.imagesSetting),
+  ];
+  int activeIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            if (activeIndex != index) {
+              setState(() {
+                activeIndex = index;
+              });
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: DrawerItem(
+              drawerItemModel: items[index],
+              isActive: activeIndex == index,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
